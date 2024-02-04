acd392ecb19e88da6cb2f877372bac8e96d6ef5f
Abhishek-coder2001 <abhishek800.roy@gmail.com>
2024-02-04 21:20:53 +0530

    progress.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+let leftArrow = document.querySelector(".left");
+let rightArrow = document.querySelector(".right");
 
+let images = document.querySelectorAll(".images");
+let currentIndex = 0;
+
+leftArrow.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  updateDisplay();
+});
+
+rightArrow.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateDisplay();
+});
+
+function updateDisplay() {
+  images.forEach((image, index) => {
+    if (index === currentIndex) {
+      image.style.display = "block";
+    } else {
+      image.style.display = "none";
+    }
+  });
+}
